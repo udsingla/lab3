52e5c43b18d64a357dcd15d307f2bd9a1109a08e
udsingla <130015308+udsingla@users.noreply.github.com>
2023-04-22 02:38:00 -0700

    list methods changed

--- ListExamples.java
+++ ListExamples.java
@@ -50,7 +50,7 @@ class ListExamples {
     }
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
-      index1 += 1;
+      index2 += 1;
     }
     return result;
   }
--- ListTests.java
+++ ListTests.java
@@ -21,7 +21,18 @@ public class ListTests {
 
     @Test
     public void testMerge(){
-        
+        List<String> input1 = new ArrayList<>();
+        input1.add("1");
+        input1.add("4");
+        input1.add("5");
+        List<String> input2 = new ArrayList<>();
+        input2.add("2");
+        input2.add("3");
+        input2.add("7");
+        List<String> expected = new ArrayList<>();
+        expected.add("1");expected.add("2");expected.add("3");
+        expected.add("4");expected.add("5");expected.add("7");
+        assertEquals(expected, ListExamples.merge(input1, input2));
     }
     
 }
